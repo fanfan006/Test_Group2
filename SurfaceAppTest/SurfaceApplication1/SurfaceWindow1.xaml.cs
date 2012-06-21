3bb75cee1eafa5edd65535ede30ce5d28ecc3168
Lin Lee <linlin9204@163.com>
2012-06-21 11:46:07 +0800

    new

--- SurfaceAppTest/SurfaceApplication1/SurfaceWindow1.xaml.cs
+++ SurfaceAppTest/SurfaceApplication1/SurfaceWindow1.xaml.cs
@@ -30,6 +30,8 @@ namespace SurfaceApplication1
         {
             InitializeComponent();
 
+            //hello
+
             // Add handlers for window availability events
             AddWindowAvailabilityHandlers();
         }
